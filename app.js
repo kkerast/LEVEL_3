ee3833d1909034fc607ed40cffe4c66e70ec375f
kkerast <dlwlsgkqkq@naver.com>
2023-05-10 23:48:24 +0900

    2023/05/10:23:48

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = 3018;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api", [usersRouter, postsRouter]);
+app.use("/", [usersRouter, postsRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트 번호로 서버가 실행되었습니다.");
